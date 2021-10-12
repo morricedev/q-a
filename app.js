94ff70874ade84e96aedc5e6cc0794c5a663aec3
Maurício Sousa <morricedev@gmail.com>
2021-10-12 09:55:03 -0300

    Config static files & add ask route

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require("express");
+const { resolve } = require("path");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const homeRoutes = require("./routes/home");
+const askRoutes = require("./routes/ask");
 
 class App {
   constructor() {
     this.app = express();
-    this.app.set("view engine", "ejs");
+    this.middlewares();
     this.routes();
   }
 
-  middlewares() {}
+  middlewares() {
+    this.app.set("view engine", "ejs");
+    this.app.use(express.static(resolve(__dirname, "public")));
+  }
 
   routes() {
     this.app.use("/", homeRoutes);
+    this.app.use("/ask/", askRoutes);
   }
 }
 
