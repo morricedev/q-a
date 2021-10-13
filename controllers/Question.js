a8ffaeff43ad6e97986eb3556f2577f385775f09
Maurício Sousa <morricedev@gmail.com>
2021-10-13 19:24:57 -0300

    Add list of responses

--- controllers/Question.js
+++ controllers/Question.js
@@ -1,14 +1,19 @@
 const Question = require("../models/Question");
+const responseController = require("../controllers/Response");
 
 class QuestionController {
   async index(req, res) {
+    req.params.question_id = req.params.id;
+
     const id = Number(req.params.id);
     if (isNaN(id)) return res.redirect("/");
 
     const question = await Question.findOne({ where: { id } });
     if (!question) return res.redirect("/");
 
-    return res.render("question", { question });
+    const responses = await responseController.index(req, res);
+
+    return res.render("question", { question, responses });
   }
 }
 
--- controllers/Response.js
+++ controllers/Response.js
@@ -1,6 +1,18 @@
 const Response = require("../models/Response");
 
 class ResponseController {
+  async index(req, res) {
+    const question_id = req.params.question_id;
+
+    const responses = await Response.findAll({
+      where: { question_id },
+      raw: true,
+      order: [["id", "DESC"]],
+    });
+
+    return responses;
+  }
+
   async store(req, res) {
     await Response.create(req.body);
 
