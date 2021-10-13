bff6a8052b88dfb6bdac91a6110aee218d357fbc
Maurício Sousa <morricedev@gmail.com>
2021-10-13 19:06:43 -0300

    Add response model & associate on connection

--- database/index.js
+++ database/index.js
@@ -1,11 +1,16 @@
 const Sequelize = require("sequelize");
 const databaseConfig = require("../config/database");
-const Question = require("../models/Question");
 
-const models = [Question];
+const Question = require("../models/Question");
+const Response = require("../models/Response");
+
+const models = [Question, Response];
 
 const connection = new Sequelize(databaseConfig);
 
 models.forEach((model) => model.init(connection));
+models.forEach(
+  (model) => model.associate && model.associate(connection.models)
+);
 
 module.exports = connection;
